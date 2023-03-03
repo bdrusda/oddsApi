a90116277d397e017dbf0cf7f3c693c6da0f7d94
Brendan <bdrusda@gmail.com>
2023-03-03 00:25:16 -0500

    Modularizing logic

--- renderer.js
+++ renderer.js
@@ -12,32 +12,25 @@ var interval = 120;
 var fontsize = tickerHeight / 2;
 
 function createRenderer(device) {
+	// Create the renderer
 	const width = device.deviceData.pixelsPerStrip;
 	const height = device.deviceData.numberStrips;
-	const canvas = document.createElement('canvas');
-	canvas.width = width * 10; // blow up everythiing x10 so we can see
-	canvas.height = height * 10;
-	document.body.appendChild(canvas);
-	const ctx = canvas.getContext('2d');
+	const ctx = createCanvas(width, height);
 
 	console.log(`Creating renderer ${width}x${height} ${MAX_FPS}fps`);
-
-	updateTickerInfo();
-
-	// Main Information
-	// renderer process
-	ipcRenderer.on('gameData', function (event, gameData) {
-		setInterval(banner, 75 / interval, ctx, gameData);
-	});
-
-	/*
-	ctx.fillStyle = 'green';
-	ctx.fillRect(0, 0, width, height);
 
 	device.startRendering(() => {
 		// Render
 		ctx.fillStyle = 'green';
 		ctx.fillRect(0, 0, width, height);
+
+		updateTickerInfo();
+
+		// Main Information
+		// renderer process
+		ipcRenderer.on('gameData', function (event, gameData) {
+			setInterval(banner, 75 / interval, ctx, gameData);
+		});
 
 		// Get data
 		const ImageData = ctx.getImageData(0, 0, width, height);
@@ -45,8 +38,19 @@ function createRenderer(device) {
 		// Send data to LEDs
 		device.setRGBABuffer(ImageData.data);
 	}, MAX_FPS);
-    */
 }
+
+// Canvas Helpers
+
+function createCanvas(width, height) {
+	const canvas = document.createElement('canvas');
+	canvas.width = width * 10; // blow up everythiing x10 so we can see
+	canvas.height = height * 10;
+	document.body.appendChild(canvas);
+	return canvas.getContext('2d');
+}
+
+// Ticker Helpers
 
 function updateTickerInfo() {
 	console.log('requesting ticker info update from main');
@@ -75,12 +79,12 @@ function banner(ctx, games) {
 	globalx += vector;
 }
 
-createRenderer({ deviceData: { pixelsPerStrip: 64, numberStrips: 64 } });
-
 /*
+createRenderer({ deviceData: { pixelsPerStrip: 64, numberStrips: 64 } });
+*/
+
 const service = new PixelPusher.Service();
 
 service.on('discover', (device) => {
 	createRenderer(device);
 });
-*/
